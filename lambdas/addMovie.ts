024541a45f9b08d0bc460d778dc326858d8afd1a
cormacwit <74902128+cormacwit@users.noreply.github.com>
2023-11-27 02:02:13 +0000

    Commit before adding Cognito

--- lambdas/addMovie.ts
+++ lambdas/addMovie.ts
@@ -73,4 +73,4 @@ function createDDbDocClient() {
   };
   const translateConfig = { marshallOptions, unmarshallOptions };
   return DynamoDBDocumentClient.from(ddbClient, translateConfig);
-}
+}////////
